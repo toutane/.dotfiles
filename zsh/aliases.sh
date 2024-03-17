94d220b2295f1948ba8c41e1b73f81b36077087d
Charles-Antoine Leger <charles-antoine.leger@epita.fr>
2024-03-17 17:05:13 +0100

    feat(skhd): open alacritty with cmd + enter

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 
-alias tree="tree -L 2"
+#alias tree="tree -L 2"
 alias gs="git status"
 alias gl="git log --oneline"
 alias ls="ls --color=auto"
-alias bat="batcat"
+# Only when on Debian
+if [ -f /etc/debian_version ]; then
+  alias bat="batcat"
+fi
+alias p="cat"
